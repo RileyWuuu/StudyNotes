42aaa5ab8ac7b2b7e20d96a12aab14516831e121
RileyWu <71340325+RileyWuuu@users.noreply.github.com>
2022-07-12 15:27:06 +0800

    Add files via upload

---
+++ fibonacciNumber/fibonacciNum.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := fibonacciNum(3)
+	fmt.Println(x)
+}
+func fibonacciNum(n int) int {
+	if n == 0 || n == 1 {
+		return n
+	}
+	return fibonacciNum(n-1) + (n + 2)
+}
