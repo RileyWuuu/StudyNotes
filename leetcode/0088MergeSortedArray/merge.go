7a9910c09e98aaa813cf7b194080db1203b4140d
RileyWu <71340325+RileyWuuu@users.noreply.github.com>
2022-07-19 16:11:13 +0800

    Update merge.go

--- leetcode/0088MergeSortedArray/merge.go
+++ leetcode/0088MergeSortedArray/merge.go
@@ -5,11 +5,13 @@ import (
 )
 
 func main() {
-	var arr []int
-	var arr2 []int
+	var arr, arr2, arr3, arr4 []int
 	arr = append(arr, 1, 2, 3, 4, 0, 0)
 	arr2 = append(arr2, 1, 7)
+	arr3 = append(arr, 1, 2, 3, 4, 0, 0)
+	arr4 = append(arr2, 1, 7)
 	merge(arr, 4, arr2, 2)
+	mergeII(arr3, 4, arr4, 2)
 }
 
 func merge(nums1 []int, m int, nums2 []int, n int) {
@@ -21,4 +23,9 @@ func merge(nums1 []int, m int, nums2 []int, n int) {
 	sort.Slice(nums1, func(i, j int) bool {
 		return nums1[i] < nums1[j]
 	})
+}
+
+func mergeII(nums1 []int, m int, nums2 []int, n int) {
+	nums1 = append(nums1[:m], nums2[:n]...)
+	sort.Ints(nums1)
 }
