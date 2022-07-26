a3815f907659991cc84deff36ce866328a1c6ce9
RileyWu <71340325+RileyWuuu@users.noreply.github.com>
2022-07-27 01:38:52 +0800

    Update twoSums.go

--- leetcode/0001TwoSums/twoSums.go
+++ leetcode/0001TwoSums/twoSums.go
@@ -8,6 +8,7 @@ func main() {
 	fmt.Println(TwoSums(inputNum, 10))
 }
 
+//Time complexity:O(n)
 func TwoSums(nums []int, target int) []int {
 	intMap := make(map[int]int)
 	for i, x := range nums {
@@ -18,4 +19,17 @@ func TwoSums(nums []int, target int) []int {
 	}
 	return []int{}
 
+}
+//Time complexity:O(n^2)
+func TwoSumsII(nums []int, target int) []int {
+	result := make([]int, 2)
+	for i := 0; i < len(nums); i++ {
+		for x := i + 1; x < len(nums); x++ {
+			if nums[i]+nums[x] == target {
+				result[0] = i
+				result[1] = x
+			}
+		}
+	}
+	return result
 }
