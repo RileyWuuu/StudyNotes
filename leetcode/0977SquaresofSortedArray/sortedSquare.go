df13cdd15aab5e91980e2673edd9f47c6642aad0
RileyWu <71340325+RileyWuuu@users.noreply.github.com>
2022-08-02 13:13:53 +0800

    Update sortedSquare.go

--- leetcode/0977SquaresofSortedArray/sortedSquare.go
+++ leetcode/0977SquaresofSortedArray/sortedSquare.go
@@ -4,20 +4,33 @@ import (
 	"fmt"
 	"sort"
 )
+//TimeComplexity:nlogn
+// func sortedSquare(nums []int) []int {
+// 	for i := 0; i < len(nums); i++ {
+// 		nums[i] = int(math.Pow(float64(nums[i]), 2))
+// 	}
+// 	sort.Ints(nums)
+// 	return nums
+// }
 
-func main() {
-	var data []int
-	data = append(data, -11, -3, 0, 5, 19)
-	fmt.Println(SortedSquare(data))
-}
+//TimeComplexity:O(n)
+// math.pow(x,2)=x二次方
+// math.abs(float(x))=負數轉正
 
-func SortedSquare(nums []int) []int {
-	for x := range nums {
-		nums[x] = nums[x] * nums[x]
+func sortedSquareII(nums []int)[]int{
+	head:=0
+	tail:=len(nums)-1
+	result := make([]int,len(nums))
+	current:=len(nums)-1
+	for current>=0{
+		if math.Abs(float(nums[head]))>=math.Abs(float(nums[tail])){
+			result[current]=int(math.Pow(float(nums[head]),2))
+			head++
+		}else{
+			result[current]=int(math.Pow(float(nums[tail]),2))
+			tail--
+		}
+		current--
 	}
-	sort.Slice(nums, func(a, b int) bool {
-		return nums[a] < nums[b]
-	})
-
-	return nums
+	return result
 }
