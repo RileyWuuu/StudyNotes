c9d1cba3daf8e8a5293030f8dc146003d6aafd74
RileyWu <71340325+RileyWuuu@users.noreply.github.com>
2022-09-23 09:42:32 +0800

    Add files via upload

---
+++ leetcode/1680/concatenatedBinary_test.go
@@ -0,0 +1,39 @@
+package p1680
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type input struct {
+	in int
+}
+type output struct {
+	out int
+}
+type result struct {
+	i input
+	o output
+}
+
+func Test_concatenatedBinary(t *testing.T) {
+	ast := assert.New(t)
+	data := []result{
+		{
+			input{1},
+			output{1},
+		},
+		{
+			input{3},
+			output{27},
+		},
+		{
+			input{12},
+			output{505379714},
+		},
+	}
+	for _, x := range data {
+		ast.Equal(x.o.out, concatenatedBinary(x.i.in))
+	}
+}
