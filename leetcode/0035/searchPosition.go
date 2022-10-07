17cd4d10be57ac3e80fdb1ad60cd8cf05a7f1b82
LGROUP\Riley.w <Riley.w@oaoooa.com>
2022-10-07 15:25:24 +0800

    leetcodeUpdate

--- leetcode/0035/searchPosition.go
+++ leetcode/0035/searchPosition.go
@@ -4,26 +4,9 @@ import (
 	"sort"
 )
 
-func main() {
-	inputInt := make([]int, 1)
-	inputInt = append(inputInt, 1, 2, 3, 4, 5, 6, 7)
-
-	SearchInsert(inputInt, 4)
-	SortSearch(inputInt,4)
-
-}
 func SearchInsert(nums []int, target int) int {
-	for i := 0; i < len(nums); i++ {
-		if nums[i] >= target {
-			return i
-		}
-	}
-	return len(nums)
-}
-func SortSearch(nums []int,target int)int{
-	i:=nums.sort.Search(len(nums)func(i int)bool{
+	i := sort.Search(len(nums), func(i int) bool {
 		return nums[i] >= target
 	})
 	return i
 }
-
